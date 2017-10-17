498213b7d02cf13f49350bb409139b90e37d9d8c
JuCN <julien.charles-nicolas@insa-lyon.fr>
2017-10-17 11:57:17 +0200

    update gameover & winner

--- predicats/gameover.pl
+++ predicats/gameover.pl
@@ -1,7 +1,8 @@
 :- ensure_loaded(../main).
 
 
-
+winner(Board,P) :- board(Board),countPawn(Board,P,Res),countPawn(Board,Player2,Res2),Res>Res2,canNotPlay(Player2).
+								   
 
 gameover(Winner) :- board(Board), winner(Board,Winner),!.
 gameover('Draw') :- board(Board),countPawn(Board,Player1,Res1),countPawn(Board,Player2,Res2), Res1==Res2. %à poursuivre
