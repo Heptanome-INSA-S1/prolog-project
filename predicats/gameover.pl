00cd6b11f525aafe85e7365d60691345b31b0ce2
Thoms971 <32865578+Thoms971@users.noreply.github.com>
2017-10-18 18:16:58 +0200

    Modification predicat winner et gameover

--- predicats/gameover.pl
+++ predicats/gameover.pl
@@ -1,8 +1,10 @@
 :- ensure_loaded(../main).
 
 
-winner(Board,P) :- board(Board),countPawn(Board,P,Res),countPawn(Board,Player2,Res2),Res>Res2,canNotPlay(Player2).
+
+winner(Board,P) :- board(Board),countPawn(Board,P,Res),countPawn(Board,Player2,Res2),Res>Res2,canNotPlay(Player2),canNotPlay(P).
 								   
 
 gameover(Winner) :- board(Board), winner(Board,Winner),!.
-gameover('Draw') :- board(Board),countPawn(Board,Player1,Res1),countPawn(Board,Player2,Res2), Res1==Res2. %à poursuivre
+gameover('Draw') :- board(Board),isBoardFull(Board),countPawn(Board,Player1,Res1),countPawn(Board,Player2,Res2), Res1==Res2.
+gameover('Draw') :- board(Board),canNotPlay(Player2),canNotPlay(Player1),countPawn(Board,Player1,Res1),countPawn(Board,Player2,Res2), Res1==Res2.
