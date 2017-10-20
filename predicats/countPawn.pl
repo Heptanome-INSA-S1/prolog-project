56e59bd9617547678f271242f8559c7668af9b34
TimGonnet <gonnet.tim@gmail.com>
2017-10-20 08:24:39 +0200

    Update countPawn.pl

--- predicats/countPawn.pl
+++ predicats/countPawn.pl
@@ -1,1 +1,6 @@
-countPawn(Board, Player, Count) :- matrix_count(Board, Player==, Count).
+isWhite(X) :- nonvar(X),X == 'W'.
+isBlack(X) :- nonvar(X),X == 'B'.
+
+countPawn(Board, WhiteCount, BlackCount) :- 
+    matrix_count(Board, isBlack, BlackCount),
+    matrix_count(Board, isWhite, WhiteCount).
