8018917234f484e61e2bfa59896520c76e138b2c
JuCN <julien.charles-nicolas@insa-lyon.fr>
2017-10-20 08:20:06 +0200

    ia.pl

--- predicats/ia.pl
+++ predicats/ia.pl
@@ -4,6 +4,6 @@ nearPawn(Board,index,color) :-
 	nth0(index, Board, Elem),
 	(nth0(index2, Board, Elem2),
 	Elem2==color,
-	(index2 is index+1; index2 is index-1; index2 is index+7; index2 is index+8; index2 is index+9;index2 is index-7; index2 is index-8; index2 is index-9 ).
+	(index2 is index+1; index2 is index-1; index2 is index+7; index2 is index+8; index2 is index+9;index2 is index-7; index2 is index-8; index2 is index-9 )).
 
 ia(Board,Player1,Move) :- repeat, Move is random(64), nearPawn(Board,Move,'B'), var(Move), !. %pas encore finit
