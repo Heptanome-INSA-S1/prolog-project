ad77b79c54ac435a579e62796f3a2761211b5926
Thoms971 <32865578+Thoms971@users.noreply.github.com>
2017-10-20 08:39:09 +0200

    Create isValidAction.pl

---
+++ predicats/isValidAction.pl
@@ -0,0 +1,4 @@
+:-ensure_loaded(matrix).
+
+isValidAction(I_Board, I_Player, [I_X,I_Y]):-matrix_get_possibilities(I_Board, I_Player, O_ListePossibilities), 
+	member([I_X,I_Y],O_ListePossibilities).
