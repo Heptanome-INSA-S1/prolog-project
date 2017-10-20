e9732923f2621eec64dc26569bda695b272ee1d1
JuCN <julien.charles-nicolas@insa-lyon.fr>
2017-10-20 09:07:08 +0200

    ia random !

--- predicats/ia.pl
+++ predicats/ia.pl
@@ -1,15 +1,9 @@
 
+choose([], []).
+choose(List, Elt) :-
+	length(List, Length),
+	random(0, Length, Index),
+	nth0(Index, List, Elt).
 
-Pawn(Board, Index) :- nth0(Index, Board, Elem), nonvar(Elem). 
-
-getPawns(Board,Player,Pawns) :- 
-
-
-nearPawn(Board,Index,Color) :-
-	nth0(Index, Board, Elem),
-	var(Elem),
-	(nth0(Index2, Board, Elem2),
-	Elem2==Color,
-	(Index2 is Index+1; Index2 is index-1; Index2 is index+7; Index2 is index+8; Index2 is index+9; Index2 is index-7; Index2 is index-8; Index2 is index-9 )).
-
-ia(Board,Player1,Player2,Move) :- repeat, Move is random(36), nearPawn(Board,Move,Player2), var(Move), !. %pas encore finit
+ia(Board,Player,Move) :-
+	matrix_get_possibilities(Board, Player,List),choose(List,Move).
