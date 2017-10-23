6b19e5aaa27b123e5e631e53af58c06832f9aa7b
TimGonnet <gonnet.tim@gmail.com>
2017-10-23 23:50:14 +0200

    Fix ia3.pl

--- predicats/ia3.pl
+++ predicats/ia3.pl
@@ -5,42 +5,40 @@
 
 :- ensure_loaded([matrix,getNumberPawnReturned]).
 
-%Change le MaxCount et BestMove si Cout>MaxCount
-nextMove(Board,Player,ListMove,MaxCount,Count,BestMove,Move) :-
-    Count>MaxCount,
-    getBestMove(Board,Player,ListMove,Count,Move).
-nextMove(Board,Player,ListMove,MaxCount,Count,BestMove,Move) :-
-    Count =< MaxCount,
-    getBestMove(Board,Player,ListMove,MaxCount,BestMove).
 
-%Calcul pour chaque move un score correspondant 
-%au (nombre de pion gagné - nombre de pion perdu), et passe aux autres coups
-getFirstBestMove(_,_,[],_,_).
-getFirstBestMove(Board,'W',[Move|ListMove],MaxCount,BestMove) :- 
-    getNumberPawnReturned(Board,'W',Move,Count),
-    CountSecond is 0,
-    gestBestMoveWithPrevisionOfSecondMove(Board,'B',Move,CountSecond),
-	nextMove(Board,'W',ListMove,MaxCount,Count-CountSecond,BestMove,Move).
-getFirstBestMove(Board,'B',[Move|ListMove],MaxCount,BestMove) :- 
-    getNumberPawnReturned(Board,'B',Move,Count),
-    CountSecond is 0,
-    gestBestMoveWithPrevisionOfSecondMove(Board,'W',Move,CountSecond),
-	nextMove(Board,'B',ListMove,MaxCount,Count-CountSecond,BestMove,Move).
+get_max_first_player(Board, Player,[I], MaxCount, I) :-
+    getNumberPawnReturned(Board, Player, I, MaxCount).
 
-%Construit un nouveau Board temporaire et essaye de deviner le coup que pourrais jouer l'IA adverse
-gestBestMoveWithPrevisionOfSecondMove(Board,Player,Move,Count) :-
-    copy_term(Board,NewBoard),  %à tester !!!!!!!!!!!!!
+
+get_max_first_player(Board, 'W',[I|T], MaxCount, BestMove) :-
+    getNumberPawnReturned(Board, 'W', I, MaxHead),
+    change_player_and_get_max_second_player(Board, 'W', I, BCount),
+    get_max(Board, 'W', T, MaxTail, MoveTail),
+    if(MaxHead - BCount > MaxTail, MaxHead - BCount, MaxTail, MaxCount),
+    if(MaxHead - BCount > MaxTail, I, MoveTail, BestMove).
+get_max_first_player(Board, 'B',[I|T], MaxCount, BestMove) :-
+    getNumberPawnReturned(Board, 'B', I, MaxHead),
+    change_player_and_get_max_second_player(Board, 'W', I, WCount),
+    get_max(Board, 'B', T, MaxTail, MoveTail),
+    if(MaxHead - WCount > MaxTail, MaxHead - WCount, MaxTail, MaxCount),
+    if(MaxHead - WCount > MaxTail, I, MoveTail, BestMove).
+    
+get_max_second_player(Board, Player,[I], MaxCount, I) :-
+    getNumberPawnReturned(Board, Player, I, MaxCount).
+
+get_max_second_player(Board, Player,[I|T], MaxCount, BestMove) :-
+    getNumberPawnReturned(Board, Player, I, MaxHead),
+    get_max(Board, Player, T, MaxTail, MoveTail),
+    if(MaxHead > MaxTail, MaxHead, MaxTail, MaxCount),
+    if(MaxHead > MaxTail, I, MoveTail, BestMove).
+	
+change_player_and_get_max_second_player(Board,Player,Move,MaxCount) :-
+    copy_term(Board,NewBoard),
     matrix_element(NewBoard,Move,Player),
     matrix_get_possibilities(NewBoard,Player,ListSecondMove),
-	getSecondBestMove(NewBoard,Player,ListSecondMove,MaxCount,BestMove). 
-
-%Retourne le BestMove de l'adversaire
-getSecondBestMove(_,_,[],_,_).
-getSecondBestMove(Board,Player,[Move|ListMove],MaxCount,BestMove) :- 
-    getNumberPawnReturned(Board,Player,Move,Count),
-	nextMove(Board,Player,ListMove,MaxCount,Count,BestMove,Move).
-
+	get_max_second_player(NewBoard,Player,ListSecondMove,MaxCount,_).
+	
+	
 ia3(Board,Player,BestMove) :-
-    MaxCount is 0,
 	matrix_get_possibilities(Board, Player,ListMove),
-	getFirstBestMove(Board,Player,ListMove,MaxCount,BestMove). 
+	get_max_first_player(Board,Player,ListMove,_,BestMove).
