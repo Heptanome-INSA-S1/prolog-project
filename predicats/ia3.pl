a40af23c57086854764dd02f9e5bb90627be6b78
TimGonnet <gonnet.tim@gmail.com>
2017-10-24 15:25:09 +0200

    Update ia3.pl

--- predicats/ia3.pl
+++ predicats/ia3.pl
@@ -10,18 +10,13 @@ get_max_first_player(Board, Player,[I], MaxCount, I) :-
     getNumberPawnReturned(Board, Player, I, MaxCount).
 
 
-get_max_first_player(Board, 'W',[I|T], MaxCount, BestMove) :-
-    getNumberPawnReturned(Board, 'W', I, MaxHead),
-    change_player_and_get_max_second_player(Board, 'W', I, BCount),
-    get_max(Board, 'W', T, MaxTail, MoveTail),
+get_max_first_player(Board, Player,[I|T], MaxCount, BestMove) :-
+    getNumberPawnReturned(Board, Player, I, MaxHead),
+    change_player_and_get_max_second_player(Board, Player, I, BCount),
+    get_max(Board, Player, T, MaxTail, MoveTail),
     if(MaxHead - BCount > MaxTail, MaxHead - BCount, MaxTail, MaxCount),
     if(MaxHead - BCount > MaxTail, I, MoveTail, BestMove).
-get_max_first_player(Board, 'B',[I|T], MaxCount, BestMove) :-
-    getNumberPawnReturned(Board, 'B', I, MaxHead),
-    change_player_and_get_max_second_player(Board, 'W', I, WCount),
-    get_max(Board, 'B', T, MaxTail, MoveTail),
-    if(MaxHead - WCount > MaxTail, MaxHead - WCount, MaxTail, MaxCount),
-    if(MaxHead - WCount > MaxTail, I, MoveTail, BestMove).
+
     
 get_max_second_player(Board, Player,[I], MaxCount, I) :-
     getNumberPawnReturned(Board, Player, I, MaxCount).
@@ -33,10 +28,13 @@ get_max_second_player(Board, Player,[I|T], MaxCount, BestMove) :-
     if(MaxHead > MaxTail, I, MoveTail, BestMove).
 	
 change_player_and_get_max_second_player(Board,Player,Move,MaxCount) :-
-    copy_term(Board,NewBoard),
-    matrix_element(NewBoard,Move,Player),
-    matrix_get_possibilities(NewBoard,Player,ListSecondMove),
-	get_max_second_player(NewBoard,Player,ListSecondMove,MaxCount,_).
+	changePlayer(Player,NextPlayer),
+	copy_term(Board,PlayedBoard),
+	matrix_element(PlayedBoard,Move,Player),
+	findIndexTransformation(PlayedBoard, Move, Indices),
+	reversePawns(PlayedBoard, Indices, NewBoard),
+    matrix_get_possibilities(NewBoard,NextPlayer,ListSecondMove),
+	get_max_second_player(NewBoard,NextPlayer,ListSecondMove,MaxCount,_).
 	
 	
 ia3(Board,Player,BestMove) :-
